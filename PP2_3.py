8839a4b37a85a9f10cf3aeb89c46cab4d49d3cbd
Johnny-Zhao-ICD2O1-01 <johnny.zhao@student.tdsb.on.ca>
2024-10-16 14:01:34 +0000

    commit

--- PP2_3.py
+++ PP2_3.py
@@ -1,8 +1,20 @@
-
+'''
+    Practice Questions: Else If
+    Author: Johnny Zhao
+    Date Created: Oct 16, 2024
+    Date Last Modified: Oct 16, 2024
+'''
 
 def q1(): 
-  #Write Assignment code here
-
+  wordQ1 = (str(input("Input a word: ")))
+  if wordQ1[-2:] == "ey":
+    print("-eys")
+  elif wordQ1[-1] == "y":
+    print("-ies")
+  elif wordQ1[-3:] == "ife":
+    print("-ives")
+  else:
+    print("-s")
 
 def q2(): 
   #Write Assignment code here
@@ -12,5 +24,5 @@ def q2():
 #Do not alter the following code
 #Comment out the following code when running your tests
 
-q1()
+  q1()
 q2()
